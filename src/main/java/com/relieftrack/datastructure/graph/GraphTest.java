26c7b2c244712881ae560fc6257dad558d9a7dd0
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:04:16 +0530

    add testAddVertex unit test

--- src/main/java/com/relieftrack/datastructure/graph/GraphTest.java
+++ src/main/java/com/relieftrack/datastructure/graph/GraphTest.java
@@ -1,5 +1,21 @@
 package com.relieftrack.datastructure.graph;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GraphTest {
-    
+
+    @Test
+    public void testAddVertex() {
+        Graph graph = new Graph();
+        Vertex v1 = new Vertex("W1", "Colombo Warehouse");
+        Vertex v2 = new Vertex("W2", "Kandy Warehouse");
+
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+
+        assertTrue(graph.getVertices().contains(v1));
+        assertTrue(graph.getVertices().contains(v2));
+        assertEquals(2, graph.getVertices().size());
+    }
 }
