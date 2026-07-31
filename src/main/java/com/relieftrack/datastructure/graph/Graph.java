8788e6485f49c4c77aefe1045a1392a087c4e9ca
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:02:41 +0530

    add Adjacency List map field and constructor to Graph

--- src/main/java/com/relieftrack/datastructure/graph/Graph.java
+++ src/main/java/com/relieftrack/datastructure/graph/Graph.java
@@ -1,5 +1,13 @@
 package com.relieftrack.datastructure.graph;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
-    
+    private final Map<Vertex, List<Edge>> adjacencyList;
+
+    public Graph() {
+        this.adjacencyList = new HashMap<>();
+    }
 }
