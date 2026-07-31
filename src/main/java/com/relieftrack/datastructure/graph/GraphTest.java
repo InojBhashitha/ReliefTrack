ec12dc83f2a910fd757d612d33044a628a62849b
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:14:45 +0530

    Add unit tests for BFS and DFS traversals

--- src/main/java/com/relieftrack/datastructure/graph/GraphTest.java
+++ src/main/java/com/relieftrack/datastructure/graph/GraphTest.java
@@ -55,4 +55,31 @@ public class GraphTest {
         assertEquals(1, edgesFromV2.size());
         assertEquals(v1, edgesFromV2.get(0).getDestination());
     }
+
+    @Test
+    public void testBFSAndDFS() {
+        Graph graph = new Graph();
+        Vertex vA = new Vertex("A", "Location A");
+        Vertex vB = new Vertex("B", "Location B");
+        Vertex vC = new Vertex("C", "Location C");
+        Vertex vD = new Vertex("D", "Location D");
+
+        // Set up a simple graph: A-B, A-C, B-D
+        graph.addUndirectedEdge(vA, vB, 1.0);
+        graph.addUndirectedEdge(vA, vC, 1.0);
+        graph.addUndirectedEdge(vB, vD, 1.0);
+
+        // BFS traversal from A
+        List<Vertex> bfsOrder = graph.bfs(vA);
+        assertEquals(4, bfsOrder.size());
+        assertEquals(vA, bfsOrder.get(0));
+        // Neighbors of A (B and C) should come before D
+        assertTrue(bfsOrder.indexOf(vB) < bfsOrder.indexOf(vD));
+        assertTrue(bfsOrder.indexOf(vC) < bfsOrder.indexOf(vD));
+
+        // DFS traversal from A
+        List<Vertex> dfsOrder = graph.dfs(vA);
+        assertEquals(4, dfsOrder.size());
+        assertEquals(vA, dfsOrder.get(0));
+    }
 }
