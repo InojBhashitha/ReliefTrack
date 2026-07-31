e8d26fa0d80105bde8cea710772e6e6b75a7ca52
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:01:25 +0530

    implement Vertex class fields and constructor

--- src/main/java/com/relieftrack/datastructure/graph/Vertex.java
+++ src/main/java/com/relieftrack/datastructure/graph/Vertex.java
@@ -1,5 +1,11 @@
 package com.relieftrack.datastructure.graph;
 
 public class Vertex {
-    
+    private String id;
+    private String name;
+
+    public Vertex(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
