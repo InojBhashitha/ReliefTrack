d8a9c074c1e29c8d5e69a512c5ad1b6f6565b671
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:01:56 +0530

    add equals, hashCode, and toString to Vertex

--- src/main/java/com/relieftrack/datastructure/graph/Vertex.java
+++ src/main/java/com/relieftrack/datastructure/graph/Vertex.java
@@ -1,5 +1,7 @@
 package com.relieftrack.datastructure.graph;
 
+import java.util.Objects;
+
 public class Vertex {
     private String id;
     private String name;
@@ -23,5 +25,23 @@ public class Vertex {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return Objects.equals(id, vertex.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + id + ")";
     }
 }
