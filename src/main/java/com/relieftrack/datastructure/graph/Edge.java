c2ac707fab0c1c7cb6fd23c578f4bd293e59b60f
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:02:08 +0530

    implement Edge class fields and constructor

--- src/main/java/com/relieftrack/datastructure/graph/Edge.java
+++ src/main/java/com/relieftrack/datastructure/graph/Edge.java
@@ -1,5 +1,13 @@
 package com.relieftrack.datastructure.graph;
 
 public class Edge {
-    
+    private Vertex source;
+    private Vertex destination;
+    private double weight;
+
+    public Edge(Vertex source, Vertex destination, double weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
 }
