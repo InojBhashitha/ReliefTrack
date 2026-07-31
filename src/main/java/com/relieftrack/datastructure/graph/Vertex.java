d889381a0bacd0c9e2634b876aa8eb2a40dc4641
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:09:14 +0530

    Create Vertex class with basic attributes

--- src/main/java/com/relieftrack/datastructure/graph/Vertex.java
+++ src/main/java/com/relieftrack/datastructure/graph/Vertex.java
@@ -1,47 +1,6 @@
 package com.relieftrack.datastructure.graph;
 
-import java.util.Objects;
-
 public class Vertex {
     private String id;
     private String name;
-
-    public Vertex(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Vertex vertex = (Vertex) o;
-        return Objects.equals(id, vertex.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return name + " (" + id + ")";
-    }
 }
