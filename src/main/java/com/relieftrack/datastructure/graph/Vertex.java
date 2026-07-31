69f086e335638d827871838889ba91eef608d00f
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:10:53 +0530

    Override equals, hashCode, and toString in Vertex class

--- src/main/java/com/relieftrack/datastructure/graph/Vertex.java
+++ src/main/java/com/relieftrack/datastructure/graph/Vertex.java
@@ -24,4 +24,22 @@ public class Vertex {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex vertex = (Vertex) o;
+        return java.util.Objects.equals(id, vertex.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + id + ")";
+    }
 }
