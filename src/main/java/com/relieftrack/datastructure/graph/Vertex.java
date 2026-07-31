16a9eee01f09cc72c6c2f3aedb95b19dfc10d7b9
Adheesha Sooriyaarachchi <106448404+adeshasur@users.noreply.github.com>
2026-08-01 00:09:26 +0530

    Add constructor to Vertex class

--- src/main/java/com/relieftrack/datastructure/graph/Vertex.java
+++ src/main/java/com/relieftrack/datastructure/graph/Vertex.java
@@ -3,4 +3,9 @@ package com.relieftrack.datastructure.graph;
 public class Vertex {
     private String id;
     private String name;
+
+    public Vertex(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
